4c310733d11e0ebcdd58d6205f6c35551ad9d02c
chiji1108 <shingo.pad@gmail.com>
2025-01-28 18:26:03 +0900

    Initial Commit

---
+++ TunnelOfCircles/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TunnelOfCircles
+//
+//  Created by 千々岩真吾 on 2025/01/28.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Model3D(named: "Scene", bundle: realityKitContentBundle)
+                .padding(.bottom, 50)
+
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview(windowStyle: .automatic) {
+    ContentView()
+}
---
+++ TunnelOfCircles/TunnelOfCirclesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TunnelOfCirclesApp.swift
+//  TunnelOfCircles
+//
+//  Created by 千々岩真吾 on 2025/01/28.
+//
+
+import SwiftUI
+
+@main
+struct TunnelOfCirclesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
