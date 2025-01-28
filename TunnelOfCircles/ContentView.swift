9850b4f6e964cdd8ee3dc6fa0854599848d11d8d
chiji1108 <shingo.pad@gmail.com>
2025-01-28 19:28:41 +0900

    Add depth

--- TunnelOfCircles/ContentView.swift
+++ TunnelOfCircles/ContentView.swift
@@ -8,38 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
-  @State private var colors: [Color] = [.cyan, .blue]
+    @State private var colors: [Color] = [.cyan, .blue]
+    @State private var depth = 0.0
 
-  let minDiameter = 50.0
-  let diameterChange = 70.0
+    let minDiameter = 50.0
+    let diameterChange = 70.0
 
-  var body: some View {
-    VStack {
-      ZStack {
-        ForEach(0..<4) { index in
-          Circle()
-            .stroke(lineWidth: 30)
-            .foregroundStyle(colors[index % 2])
-            .frame(height: minDiameter + diameterChange * Double(index))
+    var body: some View {
+        VStack {
+            ZStack {
+                ForEach(0..<4) { index in
+                    Circle()
+                        .stroke(lineWidth: 30)
+                        .foregroundStyle(colors[index % 2])
+                        .frame(height: minDiameter + diameterChange * Double(index))
+                        .padding3D(.back, depth)
+                }
+            }
+
+            Grid {
+                GridRow {
+                    Text("Depth")
+                    Slider(value: $depth, in: 0...50) {
+                        Text("Depth")
+                    }
+                }
+                GridRow {
+                    Text("Colors")
+                    HStack {
+                        ColorPicker("Colors", selection: $colors[0])
+                        ColorPicker("Colors", selection: $colors[1])
+                        Spacer()
+                    }
+                    .labelsHidden()
+                }
+            }
         }
-      }
-
-      Grid {
-        GridRow {
-          Text("Colors")
-          HStack {
-            ColorPicker("Colors", selection: $colors[0])
-            ColorPicker("Colors", selection: $colors[1])
-            Spacer()
-          }
-          .labelsHidden()
-        }
-      }
+        .padding()
     }
-    .padding()
-  }
 }
 
 #Preview(windowStyle: .automatic) {
-  ContentView()
+    ContentView()
 }
